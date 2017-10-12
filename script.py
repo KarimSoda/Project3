dfda600225e0dc040d73ddbad02e18ae3bb68b2c
jrfink.1989 <jrfink_1989@instance-1.c.fiery-webbing-179918.internal>
2017-10-12 18:26:43 +0000

    First answer complete

--- script.py
+++ script.py
@@ -3,6 +3,16 @@
 # Date: 10/12/2017
 # This is a python program that should check this log (file.https://s3.amazonaws.com/tcmg412-fall2016/http_access_log)
 
-def main (): 
-	
+def main () :
+	f = open('http_access_log', 'r')
+	count = 0
+	allLine = []
+ 
 
+	for line in f: 
+		count += 1
+		allLine.append(line)
+	print ("1. How many total request were made in the time period represented in the log?")
+	print  len(allLine)
+	
+main()
