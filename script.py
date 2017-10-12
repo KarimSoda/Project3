0bd87a3280244136aa88c996fa6f59a2c3e4c6db
jrfink.1989 <jrfink_1989@instance-1.c.fiery-webbing-179918.internal>
2017-10-12 17:43:51 +0000

    Added script

---
+++ script.py
@@ -0,0 +1,8 @@
+# Author: Karim and Jeremy
+# Course: TCMG 476
+# Date: 10/12/2017
+# This is a python program that should check this log (file.https://s3.amazonaws.com/tcmg412-fall2016/http_access_log)
+
+def main (): 
+	
+
